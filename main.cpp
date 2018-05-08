5611e7b1d2df6b405511ba0cb9f0c747233ddf6c
Student inf127314 <inf127314@lab-sec-23.cs.put.poznan.pl>
2018-05-08 09:28:33 +0200

    send and recv added

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <random>
 #define SEED 35791246
 
+#define OK 0
 #define CHCEWEJSC 1
 
 #define WEJSCIE 1
@@ -17,12 +18,10 @@ int losuj(){
   return wycieczka;
 }
 
-MPI_Status status;
-
 int main(int argc, char* argv[]) {
 
     pakiet_t rec_pkt;
-    int status;
+    MPI_Status status;
 
     MPI_Init(&argc, &argv);                 //Start MPI
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);           //get rank of node's process
@@ -43,6 +42,14 @@ int main(int argc, char* argv[]) {
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int kolejka_procesow[size];
+    int czy_weszli[size];
+    for(int i = 0; i < size; i++)
+    {
+      kolejka_procesow[i] = -1;
+      czy_weszli[i] = 0;
+    }
     srand(time(0));
     while(1) {
         int wycieczka = losuj();
@@ -57,18 +64,37 @@ int main(int argc, char* argv[]) {
               pkt.timestamp = 0;  //TODO
           		MPI_Send( &pkt, 1, MPI_PAKIET_T, i, WEJSCIE, MPI_COMM_WORLD );
           }
-          //czekaj na odpowiedz
+          //czekaj na odpowiedz od wszystkich
           while( !end )
           {
+            //TODO wysylanie do samego siebie, warunek end, 
               for (int i = 0; i < size; i++)
               {
                   MPI_Recv( &rec_pkt, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
+                  czy_weszli[status.MPI_SOURCE] = 1;
+                  if(rec_pkt->info == OK)
+                  {
+                      kolejka_procesow[status.MPI_SOURCE] = 0;
+                  }
+                  else if(rec_pkt->info == CHCEWEJSC)
+                  {
+                      kolejka_procesow[status.MPI_SOURCE] = rec_pkt->timestamp;
+                  }
               }
           }
         }
         else
         {
-
+          for (int i = 0; i < size; i++)
+          {
+              MPI_Recv( &rec_pkt, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+              if(rec_pkt->info == CHCEWEJSC)
+              {
+                packet_t pkt;
+                pkt.info = OK;
+                pkt.timestamp = 0;  //TODO
+                MPI_Send( &pkt, 1, MPI_PAKIET_T, status.MPI_SOURCE, WEJSCIE, MPI_COMM_WORLD );
+              }
+          }
         }
 }
