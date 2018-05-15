38a9bf56565a48db1b45ad327bb6e2611c46616e
Matek1731 <inf127314@lab-sec-23.cs.put.poznan.pl>
2018-05-15 09:17:56 +0200

    Lamport clock added, self sending problem solved, code refactored

--- main.cpp
+++ main.cpp
@@ -11,16 +11,24 @@
 #define WEJSCIE 1
 
 int losuj(){
-  int wycieczka = rand()%100;
-  if (wycieczka > 10){
-    wycieczka = 0;
-  }
-  return wycieczka;
+    int wycieczka = rand()%100;
+    if (wycieczka > 10)
+    {
+        wycieczka = 0;
+    }
+    return wycieczka;
 }
 
-int main(int argc, char* argv[]) {
+int max(int a, int b)
+{
+    if (a>b)  return a;
+    else  return b;
+}
 
-    pakiet_t rec_pkt;
+
+int main(int argc, char* argv[]) {
+    int zegarLamporta = 0;
+    packet_t rec_pkt;   //bylo pakiet_t ale zmienilem na packet_t bo chyba bylo zle
     MPI_Status status;
 
     MPI_Init(&argc, &argv);                 //Start MPI
@@ -30,9 +38,9 @@ int main(int argc, char* argv[]) {
 
 
     const int nitems=2;
-    int       blocklengths[2] = {1,1};
+    int blocklengths[2] = {1,1};
     MPI_Datatype typy[2] = {MPI_INT, MPI_INT};
-    MPI_Aint     offsets[2];
+    MPI_Aint offsets[2];
 
     offsets[0] = offsetof(packet_t, info);
     offsets[1] = offsetof(packet_t, timestamp);
@@ -44,57 +52,84 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     int kolejka_procesow[size];
-    int czy_weszli[size];
+    int czy_odp[size];
     for(int i = 0; i < size; i++)
     {
-      kolejka_procesow[i] = -1;
-      czy_weszli[i] = 0;
+        kolejka_procesow[i] = -1;
+        czy_odp[i] = 0;
     }
     srand(time(0));
     while(1) {
         int wycieczka = losuj();
+        //jesli przyszla wycieczka rob wszystko - wyslij CHCEWEJSC i czekaj na odpowiedzi od innych
         if (wycieczka > 0)
         {
-          //rób wszystko
-          //wyślij wszystkim "chce wejsc"
-          for (int i = 0; i < size; i++)
-          {
-          		packet_t pkt;
-              pkt.info = CHCEWEJSC;
-              pkt.timestamp = 0;  //TODO
-          		MPI_Send( &pkt, 1, MPI_PAKIET_T, i, WEJSCIE, MPI_COMM_WORLD );
-          }
-          //czekaj na odpowiedz od wszystkich
-          while( !end )
-          {
-            //TODO wysylanie do samego siebie, warunek end, 
-              for (int i = 0; i < size; i++)
-              {
-                  MPI_Recv( &rec_pkt, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-                  czy_weszli[status.MPI_SOURCE] = 1;
-                  if(rec_pkt->info == OK)
-                  {
-                      kolejka_procesow[status.MPI_SOURCE] = 0;
-                  }
-                  else if(rec_pkt->info == CHCEWEJSC)
-                  {
-                      kolejka_procesow[status.MPI_SOURCE] = rec_pkt->timestamp;
-                  }
-              }
-          }
+            //wyślij wszystkim CHCEWEJSC
+            for (int i = 0; i < size; i++)
+            {
+            	packet_t pkt;
+                pkt.info = CHCEWEJSC;
+                pkt.timestamp = zegarLamporta;  //wysylamy nasz zegarLamporta
+            	MPI_Send( pkt, 1, MPI_PAKIET_T, i, WEJSCIE, MPI_COMM_WORLD );
+            }
+            //inkrementuj zegarLamporta po broadcascie
+            zegarLamporta++;
+            //"odpowiedz" sam do siebie
+            czy_odp[rank] = 1;
+            kolejka_procesow[rank] = zegarLamporta;
+
+            //czekaj na odpowiedz od wszystkich
+            bool end = false;
+            while( !end )
+            {
+                //for (int i = 0; i < size; i++)  //ten for chyba nie jest potrzebny
+                //{
+                    //odbierz pakiet od dowolnego procesu
+                    MPI_Recv( &rec_pkt, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                    //aktualizuj zegarLamporta po Recv
+                    zegarLamporta = max(zegarLamporta, rec_pkt->timestamp) + 1;
+                    //oznacz w tablicy czy_odp ze juz przyszla odpowiedz od tego procesu
+                    czy_odp[status.MPI_SOURCE] = 1;
+                    //jesli ok to nie ma problemu
+                    if(rec_pkt->info == OK)
+                    {
+                        kolejka_procesow[status.MPI_SOURCE] = 0;
+                    }
+                    //jesli tez CHCEWEJSC przechowujemy jego timestamp
+                    else if(rec_pkt->info == CHCEWEJSC)
+                    {
+                        kolejka_procesow[status.MPI_SOURCE] = rec_pkt->timestamp;
+                    }
+                    //sprawdzamy czy mamy juz odpowiedz od wszystkich
+                    for(int j = 0; j < size; j++)
+                    {
+                        if(czy_odp[j] == 0)
+                        {
+                            break;
+                        }
+                        end = true;
+                    }
+                //}
+            }
         }
+        //jesli nie przyszla wycieczka odsylaj innym odpowiedzi
         else
         {
-          for (int i = 0; i < size; i++)
-          {
-              MPI_Recv( &rec_pkt, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-              if(rec_pkt->info == CHCEWEJSC)
-              {
-                packet_t pkt;
-                pkt.info = OK;
-                pkt.timestamp = 0;  //TODO
-                MPI_Send( &pkt, 1, MPI_PAKIET_T, status.MPI_SOURCE, WEJSCIE, MPI_COMM_WORLD );
-              }
-          }
+            for (int i = 0; i < size; i++)
+            {
+                MPI_Recv( &rec_pkt, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                //aktualizuj zegarLamporta po Recv
+                zegarLamporta = max(zegarLamporta, rec_pkt->timestamp) + 1;
+                //jesli otrzymano CHCEWEJSC odeslij OK
+                if(rec_pkt->info == CHCEWEJSC)
+                {
+                    packet_t pkt;
+                    pkt.info = OK;
+                    pkt.timestamp = zegarLamporta;
+                    MPI_Send( pkt, 1, MPI_PAKIET_T, status.MPI_SOURCE, WEJSCIE, MPI_COMM_WORLD );
+                    //inkrementuj zegarLamporta po Send
+                    zegarLamporta++;
+                }
+            }
         }
 }
