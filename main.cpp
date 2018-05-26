d51243cc5bed0f460a27734037cc6f5c7e1675a3
Makasz <makaszml@gmail.com>
2018-05-26 20:01:51 +0200

    Debug.

--- main.cpp
+++ main.cpp
@@ -74,7 +74,6 @@ int main(int argc, char* argv[]) {
     // MPI_Init(&argc, &argv);                 //Start MPI
     // MPI_Comm_rank(MPI_COMM_WORLD, &myid);           //get rank of node's process
     // MPI_Comm_size(MPI_COMM_WORLD, &nodenum);
-    printf("-1\n");
     const int nitems=2;
     int blocklengths[2] = {1,1};
     MPI_Datatype typy[2] = {MPI_INT, MPI_INT};
@@ -155,6 +154,8 @@ int main(int argc, char* argv[]) {
                             break;
                         }
                         end = true;
+                        printf("[%d] [L:%d] Przesyłam wycieczkę! \n", rank, zegarLamporta);
+                        usleep(2000000);
                     }
                 //}
             }
