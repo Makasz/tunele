41a8c24698a48f51a0afbeb997f1ab914e7b1167
Makasz <makaszml@gmail.com>
2018-05-26 15:03:16 +0200

    Debuging assigning wycieczka.

--- main.cpp
+++ main.cpp
@@ -68,7 +68,9 @@ int main(int argc, char* argv[]) {
     srand(time(0));
     while(1) {
         int wycieczka = losuj();
+        printf("[%d] [L:%d] Czy mam wycieczkę: %d", rank, zegarLamporta, wycieczka);
         //jesli przyszla wycieczka rob wszystko - wyslij CHCEWEJSC i czekaj na odpowiedzi od innych
+        wycieczka = rank % 2;
         if (wycieczka > 0)
         {
             //wyślij wszystkim CHCEWEJSC
