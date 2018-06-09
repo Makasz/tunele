f5ba2d1bcee1ba37cd55391a06d2a4bccf26db78
mlesny <mikolaj.lesny@olx.pl>
2018-06-09 13:03:51 +0200

    a

--- main.cpp
+++ main.cpp
@@ -117,7 +117,7 @@ printf("1");
 
 
 int main(int argc, char* argv[]) {
-    printf("Rozmiar podprzestrzeni: %d\n, Max osób w wycieczce: %d", argv[1], argv[2])
+    printf("Rozmiar podprzestrzeni: %d\n, Max osób w wycieczce: %d", argv[1], argv[2]);
     int zegarLamporta = 0;
     int size,rank;
     int rozmiar_podprzestrzeni = 10;
