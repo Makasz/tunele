bfbcba4475312f133b6a1f4a42bd39e77addf303
Makasz <makaszml@gmail.com>
2018-06-12 08:26:42 +0200

    Add checkign arguments to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
+echo $1 $2 $3
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
+  then
+    echo "Pass arguments in format: <max_trip_size> <size_of_subspace> <debug_mode 0/1>"
+    exit 1
+fi
 git pull
 mpic++ main.cpp
-echo $1 $2 $3
 mpirun -n 4 a.out $1 $2 $3
